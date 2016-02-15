8578c90ff186694d7361b21b4f08b47de5ba05bb
erinthered <erinbesswilliams@gmail.com>
2016-02-15 12:26:31 -0500

    add error checking

--- Lab2.cpp
+++ Lab2.cpp
@@ -11,18 +11,28 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+int main () 
+     {
      // declare and input carrying capacity, growth rate (decimal percentage), loss rate
      // (decimal percentage), initial population at year zero, and number of years to calculate.
      double carryCapacity, growthRate, lossRate, population;
      int year;
 
      cin >> carryCapacity >> growthRate >> lossRate >> population >> year; 
-
-     // calculate Verhulst formula for population growth and output results for each year as a table.
-     for (int i = 0; i < year; i++) {
-          population = ((1 + growthRate - lossRate) * population) - (growthRate * (population * population) / carryCapacity);
-          cout << i+1 << "\t" << population << endl;
+     
+     // check for negative values and end program if any are found
+     if (carryCapacity<0 || growthRate<0 || lossRate<0 || population<0 || year<0)
+     {
+          return 0;
+     }
+     else
+     {
+          // calculate Verhulst formula for population growth and output results for each year as a table.
+          for (int i = 0; i < year; i++) 
+          {
+                    population = ((1 + growthRate - lossRate) * population) - (growthRate * (population * population) / carryCapacity);
+                    cout << i+1 << "\t" << population << endl;
+          }
      }
      return 0;
 } 
